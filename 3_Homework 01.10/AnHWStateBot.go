70b72bcdff7ca05580462b31d77177d3b54b5832
Gusarov <gusarov@p310.rniirs.ru>
2021-09-09 16:45:33 +0300

    Added TelegramBot Template

--- 3_Homework 01.10/AnHWStateBot.go
+++ 3_Homework 01.10/AnHWStateBot.go
@@ -8,16 +8,13 @@ import (
 )
 
 var (
-	// глобальная переменная в которой храним токен
 	telegramBotToken string
 )
 
 func init() {
-	// принимаем на входе флаг -telegrambottoken
 	flag.StringVar(&telegramBotToken, "telegrambottoken", "", "Telegram Bot Token")
 	flag.Parse()
 
-	// без него не запускаемся
 	if telegramBotToken == "" {
 		log.Print("-telegrambottoken is required")
 		os.Exit(1)
@@ -25,7 +22,6 @@ func init() {
 }
 
 func main() {
-	// используя токен создаем новый инстанс бота
 	bot, err := tgbotapi.NewBotAPI(telegramBotToken)
 	if err != nil {
 		log.Panic(err)
@@ -33,27 +29,21 @@ func main() {
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	// u - структура с конфигом для получения апдейтов
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
-	// используя конфиг u создаем канал в который будут прилетать новые сообщения
+
 	updates, err := bot.GetUpdatesChan(u)
 
-	// в канал updates прилетают структуры типа Update
-	// вычитываем их и обрабатываем
+
 	for update := range updates {
-		// универсальный ответ на любое сообщение
 		reply := "Не знаю что сказать"
 		if update.Message == nil {
 			continue
 		}
 
-		// логируем от кого какое сообщение пришло
 		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
-		// свитч на обработку комманд
-		// комманда - сообщение, начинающееся с "/"
 		switch update.Message.Command() {
 		case "start":
 			reply = "Привет. Я телеграм-бот"
@@ -61,9 +51,8 @@ func main() {
 			reply = "world"
 		}
 
-		// создаем ответное сообщение
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
-		// отправляем
+
 		bot.Send(msg)
 	}
 }
